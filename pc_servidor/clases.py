d3ad6f43e368dc3665e11daf722829d44c875282
matias1405 <matiasalfaro1405@gmail.com>
2021-09-12 10:55:58 -0300

    creacion de modulo clases

--- pc_servidor/clases.py
+++ pc_servidor/clases.py
@@ -41,3 +41,37 @@ class Jugador():
         else:
             print("Ingreso una opcion invalida.")
             self.seguirJugando()
+
+
+class Carta():
+    def __init__(self, n, t, i):
+        self.numero = n
+        if(self.numero >= 10):
+            self.valor = 10
+        elif(self.numero == 1):
+            self.valor = 11
+        else:
+            self.valor = n
+        if(t == 0):
+            self.tipo = 'corazon'
+        elif(t == 1):
+            self.tipo = 'diamante'
+        elif(t == 2):
+            self.tipo = 'trebol'
+        else:
+            self.tipo = 'pica'
+        self.id = i
+
+class Partida():
+    def __init__(self, _cant_j, _estado):
+        self.cant_j = _cant_j
+        self.estado = _estado
+    def preguntarOtraPartida(self):
+        print("¿Desea volver a jugar? Introducza 'y' o 'n'")
+        entrada = input()
+        if(entrada == 'n' or entrada == 'y'):
+            if(entrada == 'n'):
+                self.estado = False
+        else:
+            print("Ingreso una opcion invalida.")
+            self.preguntarOtraPartida()
